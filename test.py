285523c85ffe4c230fd2603079d2d72b66ca2254
Michal <michal.rosicki@gmail.com>
2018-10-13 00:36:51 +0200

    Ignore *.pyc files

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import list_files
 
-list_files.list_files(list_files.auth('token1.json','credentials1.json'),100)
+list_files.list_files(list_files.auth('token2.json','credentials2.json'),'all')
+list_files.list_files(list_files.auth('token1.json','credentials1.json'),'all')
 
 #test
